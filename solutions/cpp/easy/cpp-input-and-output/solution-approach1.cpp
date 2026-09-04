37a256d5d502775a412fa7dd18449e625ed5eca2
Vijesh <vijeshwins.2008@gmail.com>
2026-09-04 23:38:47 +0530

    ✅ Input and Output [cpp] — approach1

---
+++ solutions/cpp/easy/cpp-input-and-output/solution-approach1.cpp
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+// Problem     Input and Output
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-04, 11:38 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+cin>>a>>b>>c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+   cout<<a+b+c;
+   return 0;
+}
