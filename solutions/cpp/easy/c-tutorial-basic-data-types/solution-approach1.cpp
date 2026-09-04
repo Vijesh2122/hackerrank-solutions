18027f2fd78861d69d1139c6ce9c5bcf974322ef
Vijesh <vijeshwins.2008@gmail.com>
2026-09-04 23:48:47 +0530

    ✅ Basic Data Types [cpp] — approach1

---
+++ solutions/cpp/easy/c-tutorial-basic-data-types/solution-approach1.cpp
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+// Problem     Basic Data Types
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-04, 11:48 p.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
+
+    return 0;
+}
