4cc998a8e93004ccd225485b36220eba047542d6
Vijesh <vijeshwins.2008@gmail.com>
2026-09-06 01:03:37 +0530

    ✅ Say "Hello, World!" With C++ [cpp] — approach1

---
+++ solutions/cpp/easy/cpp-hello-world/solution-approach1.cpp
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+// Problem     Say "Hello, World!" With C++
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-06, 01:03 a.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
